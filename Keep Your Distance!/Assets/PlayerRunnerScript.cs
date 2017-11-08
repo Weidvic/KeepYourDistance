5fe11c1fd086ae06ab5d0d161775a9cd0753d04c
Victor Weidar <Victor.weidar@gmail.com>
2017-11-08 15:09:00 +1000

    +added moveable character

---
+++ Keep Your Distance!/Assets/PlayerRunnerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRunnerScript : MonoBehaviour {
+
+    Rigidbody Rb;
+    public float Speed = 5f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float MovX = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+        transform.position += new Vector3(MovX, 0, 0);
+    }
+}
